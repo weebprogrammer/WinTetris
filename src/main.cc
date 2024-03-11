a00394e524f26a2cf15b09faa1c5ca7f28243f5e
amir <amirtagashov@gmail.com>
2024-03-11 13:03:04 +0500

    template

---
+++ src/main.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+
+#include <io.h>
+#include <windows.h>
+
+#define isatty _isatty
+
+#endif
+
+
+int main (int argc, char** argv) 
+{
+
+
+
+
+}
